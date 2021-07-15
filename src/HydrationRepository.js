ca64e2881b12f1b280eb2ce190b1204d1b792b54
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-07-15 17:50:57 -0600

    Add hydration repository class and methods

---
+++ src/HydrationRepository.js
@@ -0,0 +1,34 @@
+import Hydration from '../src/Hydration';
+
+class HydrationRepository {
+  constructor(data) {
+    this.hydration = data.map(data => new Hydration(data))
+  }
+
+  filterByUser(userID) {
+    return this.hydration.filter(data => data.userID === userID )
+  }
+
+  getTotalAvg(userID) {
+    const userHydration = this.filterByUser(userID)
+    return userHydration.reduce((currentTotal, hydration) => {
+      return hydration.numOunces + currentTotal
+    }, 0) / userHydration.length
+  }
+
+  findOuncesOfDate(userID, date) {
+    const userHydration = this.filterByUser(userID)
+    return userHydration.find(hydration => hydration.date === date).numOunces
+  }
+
+  getWeekOunces(userID, date) {
+    const userHydration = this.filterByUser(userID)
+    const reversedDates = userHydration.reverse()
+    const dateIndex = userHydration.findIndex(hydration => hydration.date === date)
+    const days = reversedDates.slice(dateIndex, dateIndex + 7)
+    return days.map(day => day.numOunces)
+   }
+
+}
+
+export default HydrationRepository;
