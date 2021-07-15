cac942a4f321f643d8330d7a9b9ae07a5f4414e7
Joana Brito <joanafiorentini@hotmail.com>
2021-07-14 18:14:18 -0700

    Add one chart to try layout

--- src/scripts.js
+++ src/scripts.js
@@ -10,6 +10,39 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
-import Chart from 'chart.js'; 
+import Chart from 'chart.js/auto'; 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+
+
+const sleepChart = document.getElementById('sleep-chart');
+console.log(Chart);
+var myChart = new Chart(sleepChart, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(255, 205, 86)'
+            ],
+            hoverOffset: 4
+          }]
+    },
+    options: {
+        cutout: '75%',
+        radius: '65%',
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
