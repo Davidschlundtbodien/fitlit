749e2d4a9ed9bc151de1652e07d03af4e27cb3d6
Joana Brito <joanafiorentini@hotmail.com>
2021-07-14 22:26:22 -0700

    Add hydration and steps chart

--- src/scripts.js
+++ src/scripts.js
@@ -16,8 +16,8 @@ import UserRepository from './UserRepository';
 
 
 const sleepChart = document.getElementById('sleep-chart');
-console.log(Chart);
-var myChart = new Chart(sleepChart, {
+
+let mySleepChart = new Chart(sleepChart, {
     type: 'doughnut',
     data: {
         labels: [
@@ -29,9 +29,73 @@ var myChart = new Chart(sleepChart, {
             label: 'My First Dataset',
             data: [300, 50, 100],
             backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)'
+              '#300264f5',
+              '#77777777',
+              '#0fa2b6f2'
+            ],
+            hoverOffset: 4
+          }]
+    },
+    options: {
+        cutout: '75%',
+        radius: '65%',
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
+
+const hydrationChart = document.getElementById('hydration-chart');
+
+let myHydrationChart = new Chart(hydrationChart, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+              '#300264f5',
+              '#77777777',
+              '#0fa2b6f2'
+            ],
+            hoverOffset: 4
+          }]
+    },
+    options: {
+        cutout: '75%',
+        radius: '65%',
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
+
+const stepsChart = document.getElementById('steps-chart');
+
+let myStepsChart = new Chart(stepsChart, {
+    type: 'doughnut',
+    data: {
+        labels: [
+            'Red',
+            'Blue',
+            'Yellow'
+          ],
+          datasets: [{
+            label: 'My First Dataset',
+            data: [300, 50, 100],
+            backgroundColor: [
+              '#300264f5',
+              '#77777777',
+              '#0fa2b6f2'
             ],
             hoverOffset: 4
           }]
