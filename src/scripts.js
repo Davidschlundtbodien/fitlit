bc9353c3ecfd6224674ee1173ea6d53e16930314
Joana Brito <joanafiorentini@hotmail.com>
2021-07-15 19:06:17 -0700

    Import files and start friends chart

--- src/scripts.js
+++ src/scripts.js
@@ -10,103 +10,67 @@ import './images/turing-logo.png'
 console.log('This is the JavaScript entry file - your code begins here.');
 
 // An example of how you tell webpack to use a JS file
+import datepicker from 'js-datepicker';
+import { myStepsChart, myHydrationChart,mySleepChart } from './data/user-charts';
 import Chart from 'chart.js/auto'; 
 import userData from './data/users';
 import UserRepository from './UserRepository';
 
 
-const sleepChart = document.getElementById('sleep-chart');
+const calendar = datepicker('#calendar-input');
+// const calendarIcon = document.querySelector('#calendar img');
 
-let mySleepChart = new Chart(sleepChart, {
-    type: 'doughnut',
+// calendarIcon.addEventListener('click', (event) => {
+//     event.stopPropagation();
+//     calendar.show();
+//     console.log("im here!!");
+// });
+
+const friendsChart = document.getElementById('friends-avarage-chart');
+
+const friendlabels = ['friend1', 'friend2', 'friend3','friend4','friend5'];
+
+let userFriendsChart = new Chart(friendsChart, {
+    type: 'bar',
     data: {
-        labels: [
-            'Red',
-            'Blue',
-            'Yellow'
+        labels: friendlabels,
+        datasets: [{
+          label: 'My friends Steps',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
           ],
-          datasets: [{
-            label: 'My First Dataset',
-            data: [300, 50, 100],
-            backgroundColor: [
-              '#300264f5',
-              '#77777777',
-              '#0fa2b6f2'
-            ],
-            hoverOffset: 4
-          }]
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }],
     },
     options: {
-        cutout: '75%',
-        radius: '65%',
         plugins: {
             legend: {
                 display: false,
             }
-        }
-    }
-});
-
-const hydrationChart = document.getElementById('hydration-chart');
-
-let myHydrationChart = new Chart(hydrationChart, {
-    type: 'doughnut',
-    data: {
-        labels: [
-            'Red',
-            'Blue',
-            'Yellow'
-          ],
-          datasets: [{
-            label: 'My First Dataset',
-            data: [300, 50, 100],
-            backgroundColor: [
-              '#300264f5',
-              '#77777777',
-              '#0fa2b6f2'
-            ],
-            hoverOffset: 4
-          }]
-    },
-    options: {
-        cutout: '75%',
-        radius: '65%',
-        plugins: {
-            legend: {
-                display: false,
+        },
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
             }
-        }
-    }
-});
-
-const stepsChart = document.getElementById('steps-chart');
-
-let myStepsChart = new Chart(stepsChart, {
-    type: 'doughnut',
-    data: {
-        labels: [
-            'Red',
-            'Blue',
-            'Yellow'
-          ],
-          datasets: [{
-            label: 'My First Dataset',
-            data: [300, 50, 100],
-            backgroundColor: [
-              '#300264f5',
-              '#77777777',
-              '#0fa2b6f2'
-            ],
-            hoverOffset: 4
-          }]
-    },
-    options: {
-        cutout: '75%',
-        radius: '65%',
-        plugins: {
-            legend: {
-                display: false,
-            }
-        }
+      },
     }
 });
